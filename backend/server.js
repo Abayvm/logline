3c3241760fa60e324a87818cfaec1fe77d8837b0
Abayvm <abayvmuraleedharan@gmail.com>
2025-07-18 20:09:18 +0530

    initial backend setup: express, MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jsonWebToken = require('jsonwebtoken');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, ()=>{
+    console.log(`server online at port${PORT}`);
+})
