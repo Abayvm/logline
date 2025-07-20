be11b5e60a9bbe3757cb937ec010e6a2537be1cd
Abayvm <abayvmuraleedharan@gmail.com>
2025-07-20 15:27:15 +0530

    add slug support to generate clean URL from topic

--- backend/models/Loglist.js
+++ backend/models/Loglist.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const LoglistSchema = new mongoose.Schema({
     topic : {type: String, required: true},
     description: {type: String},
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
 },{timestamps: true});
+
+LoglistSchema.pre('validate', function(next) {
+    if (this.topic) {
+        this.slug = slugify(this.topic, { lower: true, strict: true });
+    }
+    next();
+});
 
 module.exports = mongoose.model("Loglist", LoglistSchema);
